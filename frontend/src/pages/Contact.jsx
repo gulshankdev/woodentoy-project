e062d3f067bfca03e9664a7b413f6fd1bfbf6dbb
gulshan kumar <gulshanneya515@gmail.com>
2026-07-28 17:46:35 +0530

    commit

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,7 +1,7 @@
-import ContactHero from "../components/contact/ContactHero";
-import ContactSection from "../components/contact/ContactSection";
-import ContactMap from "../components/contact/ContactMap";
-import FAQ from "../components/contact/FAQ";
+import ContactHero from "../components/Contact/ContactHero";
+import ContactSection from "../components/Contact/ContactSection";
+import ContactMap from "../components/Contact/ContactMap";
+import FAQ from "../components/Contact/FAQ";
 
 function Contact() {
   return (
